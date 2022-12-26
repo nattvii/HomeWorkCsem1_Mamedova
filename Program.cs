a69346370ca980855315ba0bf426ed19e7881640
nattvii <natt.mmd@gmail.com>
2022-12-26 10:10:58 +0300

    ex_03

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+// Convert.ToInt32(Console.Readline());
+
+Console.WriteLine("Fill in a number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+   Console.WriteLine("Even");
+else 
+   Console.WriteLine("Odd number");
